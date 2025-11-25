4215705a78c532f5dcff77a13f3d656e1ba95171
Jl Sut Pi <sutpi070@gmail.com>
2025-11-25 10:51:56 -0300

    group checkout page

--- ecommerce/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce/src/pages/checkout/CheckoutPage.jsx
@@ -1,34 +1,12 @@
-import { Link } from "react-router";
 import "./CheckoutPage.css";
-import "./checkout-header.css";
+import { CheckoutHeader } from "./CheckoutHeader";
 
 function CheckoutPage() {
   return (
     <>
       <title>Checkout</title>
+      <CheckoutHeader />
 
-      <div className="checkout-header">
-        <div className="header-content">
-          <div className="checkout-header-left-section">
-            <Link to="/">
-              <img className="logo" src="images/logo.png" />
-              <img className="mobile-logo" src="images/mobile-logo.png" />
-            </Link>
-          </div>
-
-          <div className="checkout-header-middle-section">
-            Checkout (
-            <Link className="return-to-home-link" to="/">
-              3 items
-            </Link>
-            )
-          </div>
-
-          <div className="checkout-header-right-section">
-            <img src="images/icons/checkout-lock-icon.png" />
-          </div>
-        </div>
-      </div>
       <div className="checkout-page">
         <div className="page-title">Review your order</div>
 
@@ -70,7 +48,7 @@ function CheckoutPage() {
                   <div className="delivery-option">
                     <input
                       type="radio"
-                      checked
+                      defaultChecked
                       className="delivery-option-input"
                       name="delivery-option-1"
                     />
@@ -165,7 +143,7 @@ function CheckoutPage() {
                   <div className="delivery-option">
                     <input
                       type="radio"
-                      checked
+                      defaultChecked
                       className="delivery-option-input"
                       name="delivery-option-2"
                     />
