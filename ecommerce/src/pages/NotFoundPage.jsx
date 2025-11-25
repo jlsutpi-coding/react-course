55363701b775426f277d3361830ff4a785789ae0
Jl Sut Pi <sutpi070@gmail.com>
2025-11-25 21:48:16 +0700

    add not found page

---
+++ ecommerce/src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+import "./NotFoundPage.css";
+function NotFoundPage() {
+  return (
+    <>
+      <Header />
+      <div className="not-found-page">Not Found Page</div>
+    </>
+  );
+}
+
+export default NotFoundPage;
