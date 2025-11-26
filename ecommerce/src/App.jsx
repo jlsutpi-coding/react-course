6aeb9f2a5b56f6bb9e6475347280c50888475184
Jl Sut Pi <sutpi070@gmail.com>
2025-11-26 21:31:02 +0700

    separate into smaller components

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
-import HomePage from "./pages/HomePage";
-import OrdersPage from "./pages/OrdersPage";
+import HomePage from "./pages/home/HomePage";
 import CheckoutPage from "./pages/checkout/CheckoutPage";
 import TrackingPage from "./pages/TrackingPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import OrdersPage from "./pages/orders/OrdersPage";
 
 function App() {
   const [cart, setCart] = useState([]);
