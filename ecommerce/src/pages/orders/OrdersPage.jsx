bb364bcbdebe35aa59af5868974586a89e2ac197
Jl Sut Pi <sutpi070@gmail.com>
2025-11-27 21:12:53 +0700

    add DeliveryDate and CartItemDetail compo

--- ecommerce/src/pages/orders/OrdersPage.jsx
+++ ecommerce/src/pages/orders/OrdersPage.jsx
@@ -10,9 +10,11 @@ function OrdersPage({ cart }) {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    axios
-      .get("/api/orders?expand=products")
-      .then((response) => setOrders(response.data));
+    const fetchData = async () => {
+      const response = await axios.get("/api/orders?expand=products");
+      setOrders(response.data);
+    };
+    fetchData();
   }, []);
 
   return (
