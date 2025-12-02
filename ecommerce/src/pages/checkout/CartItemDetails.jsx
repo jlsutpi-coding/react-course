1d4fce72edf565768e7613a1b0c7fefc0f07753f
Jl Sut Pi <sutpi070@gmail.com>
2025-12-02 23:44:04 +0700

    handle quantity update in checkout page

--- ecommerce/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce/src/pages/checkout/CartItemDetails.jsx
@@ -1,11 +1,66 @@
 import axios from "axios";
 import { formatMoney } from "../../utils/money";
+import { useEffect, useRef, useState } from "react";
 
 export function CartItemDetail({ cartItem, loadCart }) {
+  const inputRef = useRef(null);
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [quantity, setQuantity] = useState(cartItem.quantity);
+
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await loadCart();
   };
+
+  useEffect(() => {
+    if (isUpdating && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isUpdating]);
+
+  useEffect(() => {
+    // Close when clicking outside
+    function handleClickOutside(e) {
+      if (
+        isUpdating &&
+        inputRef.current &&
+        !inputRef.current.contains(e.target)
+      ) {
+        setIsUpdating(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [isUpdating]);
+
+  const onUpdateHandler = async () => {
+    if (isUpdating) {
+      await axios.put(`/api/cart-items/${cartItem.productId}`, {
+        quantity: Number(quantity),
+      });
+      loadCart();
+    }
+    setIsUpdating(!isUpdating);
+  };
+
+  const onKeyDownHandler = async (e) => {
+    inputRef.current.focus();
+
+    if (e.key === "Enter") {
+      if (isUpdating) {
+        await axios.put(`/api/cart-items/${cartItem.productId}`, {
+          quantity: Number(quantity),
+        });
+        loadCart();
+        setIsUpdating(!isUpdating);
+      }
+    }
+    if (e.key === "Escape") {
+      setIsUpdating(!isUpdating);
+    }
+  };
+
   return (
     <>
       <img className="product-image" src={cartItem.product.image} />
@@ -17,10 +72,26 @@ export function CartItemDetail({ cartItem, loadCart }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity:{" "}
-            <span className="quantity-label">{cartItem.quantity}</span>
+            Quantity:
+            {isUpdating ? (
+              <input
+                type="text"
+                ref={inputRef}
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+                onKeyDown={(e) => onKeyDownHandler(e)}
+                style={{ width: "50px" }}
+              />
+            ) : (
+              <span className="quantity-label">{cartItem.quantity}</span>
+            )}
           </span>
-          <span className="update-quantity-link link-primary">Update</span>
+          <span
+            onClick={onUpdateHandler}
+            className="update-quantity-link link-primary"
+          >
+            Update
+          </span>
           <span
             className="delete-quantity-link link-primary"
             onClick={deleteCartItem}
