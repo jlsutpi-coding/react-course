d45e0f36fb9da73191c288ea9c69fc9b375e15d6
Jl Sut Pi <sutpi070@gmail.com>
2025-12-05 21:01:44 +0700

    test with 0 and -999 in formatMoney

--- ecommerce/src/utils/money.js
+++ ecommerce/src/utils/money.js
@@ -1,1 +1,7 @@
-export const formatMoney = (priceCents) => `$${(priceCents / 100).toFixed(2)}`;
+export const formatMoney = (priceCents) => {
+  if (priceCents < 0) {
+    priceCents = priceCents * -1;
+    return `-$${(priceCents / 100).toFixed(2)}`;
+  }
+  return `$${(priceCents / 100).toFixed(2)}`;
+};
